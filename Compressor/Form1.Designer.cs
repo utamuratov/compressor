914366c182f19474b702b6eee2f450044d29c2c7
utamuratov <43562558+utamuratov@users.noreply.github.com>
2023-04-29 08:38:40 +0500

    all jobs

---
+++ Compressor/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Compressor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnAddingFile = new MetroFramework.Controls.MetroButton();
+            this.btnCompressor = new MetroFramework.Controls.MetroButton();
+            this.btnDecompressor = new MetroFramework.Controls.MetroButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.GridFileFullName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GridFileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GridFileType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GridFileSize = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GridFileModified = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "*.*";
+            this.openFileDialog1.Title = "Choosing file";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.White;
+            this.flowLayoutPanel1.Controls.Add(this.tableLayoutPanel1);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 63);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(690, 98);
+            this.flowLayoutPanel1.TabIndex = 8;
+            // 
+            // btnAddingFile
+            // 
+            this.btnAddingFile.BackColor = System.Drawing.Color.Transparent;
+            this.btnAddingFile.BackgroundImage = global::Compressor.Properties.Resources.file_add_512_3;
+            this.btnAddingFile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnAddingFile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddingFile.Location = new System.Drawing.Point(3, 3);
+            this.btnAddingFile.Name = "btnAddingFile";
+            this.btnAddingFile.Size = new System.Drawing.Size(78, 68);
+            this.btnAddingFile.TabIndex = 3;
+            this.btnAddingFile.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAddingFile.UseSelectable = true;
+            this.btnAddingFile.Click += new System.EventHandler(this.btnAddingFile_Click);
+            // 
+            // btnCompressor
+            // 
+            this.btnCompressor.BackColor = System.Drawing.Color.Transparent;
+            this.btnCompressor.BackgroundImage = global::Compressor.Properties.Resources.archives_2;
+            this.btnCompressor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnCompressor.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCompressor.Location = new System.Drawing.Point(86, 3);
+            this.btnCompressor.Name = "btnCompressor";
+            this.btnCompressor.Size = new System.Drawing.Size(77, 68);
+            this.btnCompressor.TabIndex = 4;
+            this.btnCompressor.UseSelectable = true;
+            this.btnCompressor.Click += new System.EventHandler(this.btnCompressor_Click);
+            // 
+            // btnDecompressor
+            // 
+            this.btnDecompressor.BackColor = System.Drawing.Color.Transparent;
+            this.btnDecompressor.BackgroundImage = global::Compressor.Properties.Resources.unarchiver_2;
+            this.btnDecompressor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnDecompressor.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDecompressor.Location = new System.Drawing.Point(169, 3);
+            this.btnDecompressor.Name = "btnDecompressor";
+            this.btnDecompressor.Size = new System.Drawing.Size(78, 68);
+            this.btnDecompressor.TabIndex = 5;
+            this.btnDecompressor.UseSelectable = true;
+            this.btnDecompressor.Click += new System.EventHandler(this.btnDecompressor_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.GridFileFullName,
+            this.GridFileName,
+            this.GridFileType,
+            this.GridFileSize,
+            this.GridFileModified});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 164);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(690, 229);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.VirtualMode = true;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // GridFileFullName
+            // 
+            this.GridFileFullName.HeaderText = "File Full Name";
+            this.GridFileFullName.Name = "GridFileFullName";
+            this.GridFileFullName.ReadOnly = true;
+            this.GridFileFullName.Visible = false;
+            // 
+            // GridFileName
+            // 
+            this.GridFileName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.GridFileName.HeaderText = "Name";
+            this.GridFileName.Name = "GridFileName";
+            this.GridFileName.ReadOnly = true;
+            // 
+            // GridFileType
+            // 
+            this.GridFileType.HeaderText = "File type";
+            this.GridFileType.Name = "GridFileType";
+            this.GridFileType.ReadOnly = true;
+            this.GridFileType.Width = 150;
+            // 
+            // GridFileSize
+            // 
+            this.GridFileSize.HeaderText = "Size";
+            this.GridFileSize.Name = "GridFileSize";
+            this.GridFileSize.ReadOnly = true;
+            this.GridFileSize.Width = 150;
+            // 
+            // GridFileModified
+            // 
+            this.GridFileModified.HeaderText = "Modified date";
+            this.GridFileModified.Name = "GridFileModified";
+            this.GridFileModified.ReadOnly = true;
+            this.GridFileModified.Width = 150;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "just";
+            this.saveFileDialog1.FileName = "*.just";
+            this.saveFileDialog1.Title = "Saving files as a compressed file";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "*.*";
+            this.openFileDialog2.Title = "Choose a compressed file";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Choose directory for saving file(s)";
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.metroLabel1.Location = new System.Drawing.Point(4, 75);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(76, 17);
+            this.metroLabel1.TabIndex = 11;
+            this.metroLabel1.Text = "Adding file";
+            this.metroLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.metroLabel2.Location = new System.Drawing.Point(90, 75);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(78, 14);
+            this.metroLabel2.TabIndex = 12;
+            this.metroLabel2.Text = "Compressing";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33499F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33168F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.btnCompressor, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.metroLabel3, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnAddingFile, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.metroLabel2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.metroLabel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnDecompressor, 3, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.19889F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.80111F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(261, 92);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.metroLabel3.Location = new System.Drawing.Point(169, 75);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(73, 15);
+            this.metroLabel3.TabIndex = 13;
+            this.metroLabel3.Text = "Uncompressing";
+            this.metroLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroLabel3.Click += new System.EventHandler(this.metroLabel3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 416);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Compressor";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private MetroFramework.Controls.MetroButton btnAddingFile;
+        private MetroFramework.Controls.MetroButton btnCompressor;
+        private MetroFramework.Controls.MetroButton btnDecompressor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GridFileFullName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GridFileName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GridFileType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GridFileSize;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GridFileModified;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Compressor/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Import the Material Skin
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+//Import Metrodesign
+using MetroFramework.Forms;
+using MetroFramework;
+using System.IO;
+
+namespace Compressor
+{
+    public partial class Form1 : MetroForm
+    {
+        List<FileStream> files = new List<FileStream>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCompressor_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(saveFileDialog1.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+                MetroMessageBox.Show(this, "Compressing has been successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception)
+            {
+                MetroMessageBox.Show(this, "Something has been wrong. Please, try again");
+                throw;
+            }
+        }
+
+        private void btnAddingFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialog1.FileName.Length > 0)
+                {
+                    openFileDialog1.FileName = "*.*";
+                }
+                openFileDialog1.ShowDialog();
+            }
+            catch (Exception)
+            {
+                MetroMessageBox.Show(this, "Something has been wrong. Please, try again");
+                throw;
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                string fileName = "";
+                string fileType;
+                DateTime fileModified;
+                double fileSize;
+                string fileFullName;
+                if (openFileDialog1.CheckFileExists)
+                {
+                    fileName = openFileDialog1.FileName;
+                    if (File.Exists(fileName))
+                    {
+                        fileFullName = fileName;
+                        fileModified = File.GetLastAccessTime(fileName);
+                        FileStream fs = File.Open(fileName, FileMode.OpenOrCreate);
+                        files.Add(fs);
+                        fileSize = ((double)fs.Length) / 1024;
+                        fileName = fileName.Substring(fileName.LastIndexOf("\\") + 1);
+                        fileType = fileName.Substring(fileName.LastIndexOf(".") + 1);
+                        fileName = fileName.Substring(0, fileName.LastIndexOf("."));
+                        dataGridView1.Rows.Add(fileFullName, fileName, fileType, Math.Round(fileSize, 1) + " KB", fileModified);
+
+                        fs.Close();
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                MetroMessageBox.Show(this, "Something has been wrong. Please, try again");
+                throw;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                FileStream nF = new FileStream(saveFileDialog1.FileName, FileMode.Create);
+                int byt;
+                string fileShortName;
+                int fileLengthDigitsCount;
+                long fileLength;
+
+                nF.WriteByte((byte)files.Count);
+
+                foreach (FileStream file in files)
+                {
+                    FileStream fs = new FileStream(file.Name, FileMode.Open);
+                    fileShortName = GetFileShortNameWithExt(file.Name);
+                    nF.WriteByte((byte)fileShortName.Length);
+                    for (int i = 0; i < fileShortName.Length; i++)
+                    {
+                        nF.WriteByte((byte)(int)(char)fileShortName[i]);
+                    }
+
+                    fileLength = fs.Length;
+                    fileLengthDigitsCount = fileLength.ToString().Length;
+                    nF.WriteByte((byte)fileLengthDigitsCount);
+                    for (int k = 0; k < fileLengthDigitsCount; k++)
+                    {
+                        nF.WriteByte((byte)(fileLength % 10));
+                        fileLength /= 10;
+                    }
+
+                    byt = fs.ReadByte();
+                    while (byt != -1)
+                    {
+                        nF.WriteByte((byte)byt);
+                        byt = fs.ReadByte();
+                    }
+                    fs.Close();
+                }
+
+                nF.Close();
+                dataGridView1.Rows.Clear();
+            }
+            catch (Exception)
+            {
+                MetroMessageBox.Show(this, "Something has been wrong. Please, try again.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                throw;
+            }
+        }
+
+        private string GetFileShortName(string fileName)
+        {
+            fileName = fileName.Substring(fileName.LastIndexOf("\\") + 1);
+            fileName = fileName.Substring(0, fileName.LastIndexOf("."));
+            return fileName;
+        }
+
+        private string GetFileShortNameWithExt(string fileName)
+        {
+            fileName = fileName.Substring(fileName.LastIndexOf("\\") + 1);
+            return fileName;
+        }
+
+        private void btnDecompressor_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialog2.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+                MetroMessageBox.Show(this, "Decompressing has been successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception)
+            {
+                MetroMessageBox.Show(this, "Something has been wrong. Please, try again.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                throw;
+            }
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            using (folderBrowserDialog1)
+            {
+                if (folderBrowserDialog1.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+
+                try
+                {
+                    FileStream cF = new FileStream(openFileDialog2.FileName, FileMode.Open);
+                    int byt;
+
+                    int filesCount = cF.ReadByte();
+                    int fileShortNameCount;
+                    string fileShortName = "";
+                    long fileLength;
+                    int fileLengthDigitsCount;
+                    int d;
+                    string fileNewPathRoot;
+                    for (int i = 0; i < filesCount; i++)
+                    {
+                        fileShortName = "";
+                        fileShortNameCount = cF.ReadByte();
+                        for (int j = 0; j < fileShortNameCount; j++)
+                        {
+                            fileShortName += ((char)cF.ReadByte()).ToString();
+                        }
+                        if (folderBrowserDialog1.SelectedPath.Length > 0)
+                        {
+                            fileNewPathRoot = folderBrowserDialog1.SelectedPath;
+                        }
+                        else
+                        {
+                            fileNewPathRoot = openFileDialog2.FileName.Substring(0, openFileDialog2.FileName.LastIndexOf("\\") + 1);
+                        }
+                        FileStream fs = new FileStream(fileNewPathRoot + fileShortName, FileMode.Create);
+
+                        fileLength = 0;
+                        fileLengthDigitsCount = cF.ReadByte();
+                        for (int k = 0; k < fileLengthDigitsCount; k++)
+                        {
+                            d = cF.ReadByte();
+                            fileLength += d * (long)(Math.Pow(10, k));
+                        }
+
+                        for (long t = 0; t < fileLength; t++)
+                        {
+                            byt = cF.ReadByte();
+                            fs.WriteByte((byte)byt);
+                        }
+
+
+                        //byt = cF.ReadByte();
+                        //while (byt != -1)
+                        //{
+                        //    fs.WriteByte((byte)byt);
+                        //    byt = cF.ReadByte();
+                        //}
+                        fs.Close();
+                    }
+
+                    cF.Close();
+                }
+                catch (Exception)
+                {
+                    MetroMessageBox.Show(this, "Something has been wrong. Please, try again.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    throw;
+                }
+            }
+        }
+
+        private void metroLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
